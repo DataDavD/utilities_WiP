49dfbf39d50335c9956dd391464b8e15f23d95d8
David Dansby <ddansby89@gmail.com>
2019-10-25 20:53:10 -0700

    - small changes

--- ecdf.py
+++ ecdf.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def ecdf(data):
     """Compute ECDF for a one-dimensional array of measurements."""
     # Number of data points: n
-	n = len(data)
+    n = len(data)
 
     # x-data for the ECDF: x
-	x = np.sort(data)
+    x = np.sort(data)
 
     # y-data for the ECDF: y
     y = np.arange(1, n+1) / n
